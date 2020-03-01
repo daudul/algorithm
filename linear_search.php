4630689adf1fa4b4796ea2831fbd4df2ce7b302b
daud <guest.batworld@gmail.com>
2020-03-01 16:30:44 +0600

    modify

--- linear_search.php
+++ linear_search.php
@@ -1,8 +1,7 @@
 <?php
 function LinearSearch($arr, $target){
-    $l=0;
     for ($i = 0; $i< count($arr); $i++){
-        echo $l++;
+        //mach target value
         if ($arr[$i] == $target){
             return $arr[$i];
         }
@@ -10,7 +9,7 @@ function LinearSearch($arr, $target){
     return -1;
 }
 
-$a = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,29,30,19,1];
+$arr = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,26,27,28,29,30,19,1];
 $target = 1;
-$r = LinearSearch($a, $target);
-echo $r;
+echo LinearSearch($arr, $target);
+
